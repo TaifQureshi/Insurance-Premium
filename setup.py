a9f2ae53fddf07179180b057a87c30f2d69bedee
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2023-02-03 18:51:46 +0530

    data processing update

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup,find_packages
 
 
-setup(name="src",version="1.0",description="my intenship project using MLOPS",packages=find_packages(),
+setup(name="src",version="1.0",description="my intenship project",packages=find_packages(),
       license="MIT")
