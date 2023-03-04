99db5dfad91f62f9152e5e6ebb105aa8beab8a50
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2023-03-04 12:09:36 +0530

    Main runner file created

--- src/set_logger.py
+++ src/set_logger.py
@@ -1,9 +1,10 @@
 import logging
 import os
 import yaml
+import sys
 
 
-def set_logger(file_name):
+def set_logger(file_name, stdout=False):
     base_dir = os.path.dirname(os.path.dirname(__file__))
     config = yaml.safe_load(open(os.path.join(base_dir, 'params.yaml'), 'r'))
     log_path = config.get('log_dir')
@@ -15,6 +16,9 @@ def set_logger(file_name):
 
     formatter = logging.Formatter('%(asctime)s - %(name)10s - %(levelname)7s - %(message)s')
     handlers = [logging.FileHandler(filename=f"{base_dir}/{file_name}.log")]
+
+    if stdout:
+        handlers.append(logging.StreamHandler(sys.stdout))
 
     for handler in handlers:
         handler.setLevel(logging.INFO)
