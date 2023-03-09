f2cef40db5922ed4e613880b0602bd073cd531a8
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2023-03-09 12:17:53 +0530

    app.py update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import logging
 from src.set_logger import set_logger
 from src.load_model import load_model
@@ -26,23 +26,29 @@ encoder = load_encoder()
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        age = request.form.get("age")
-        region = request.form.get("region")
-        bmi = request.form.get("bmi")
-        children = request.form.get("children")
-        sex = request.form.get("gender")
-        smoker = request.form.get("smoker")
-        df = pd.DataFrame([[sex, smoker, region]], columns=['sex', 'smoker', 'region'])
-        df = encoder.transform(df).toarray()
-        df = pd.DataFrame(df, columns=encoder.get_feature_names_out(['sex', 'smoker', 'region']))
-        data = pd.DataFrame([[children, age, bmi]], columns=['children', 'age_bin', 'bmi_bin'])
-        data = pd.concat([data, df], axis=1)
-        output = model.predict(data)
-        return render_template(r"index.html", output=round(output[0], 3))
+        try:
+            age = request.form.get("age")
+            region = request.form.get("region")
+            bmi = request.form.get("bmi")
+            children = request.form.get("children")
+            sex = request.form.get("gender")
+            smoker = request.form.get("smoker")
+            df = pd.DataFrame([[sex, smoker, region]], columns=['sex', 'smoker', 'region'])
+            df = encoder.transform(df).toarray()
+            df = pd.DataFrame(df, columns=encoder.get_feature_names_out(['sex', 'smoker', 'region']))
+            data = pd.DataFrame([[children, age, bmi]], columns=['children', 'age_bin', 'bmi_bin'])
+            data = pd.concat([data, df], axis=1)
+            output = model.predict(data)
+            return render_template(r"index.html", output=round(output[0], 3))
+        except Exception as e:
+            logger.info("Error while prediction")
+            logger.exception(e)
+            return redirect(url_for('page_not_found'))
     return render_template(r"index.html")
 
 
 @app.errorhandler(404)
+@app.route('/error', methods=["POST", "GET"])
 def page_not_found(e):
     return render_template(r"error_404.html"), 404
 
